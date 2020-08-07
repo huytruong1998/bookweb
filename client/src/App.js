18e6387f74e9c03a264c98201f861d9fa413761b
Huy <skyzerzlol@gmail.com>
2020-08-07 14:00:17 +0300

    add NavBAr and Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Landing from "./components/layout/Landing/Landing";
+import Navbar from "./components/layout/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <div className="container">
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
